99e8bc14ea02b24d4ce2bbe9cb83505f20673813
Luis Del Giudice <luis.dg19@gmail.com>
2015-04-30 22:17:08 -0400

    Created articulo and articulo tests

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from sistema.models import SistemaModel
 
-# Create your models here.
+
+class Articulo(SistemaModel):
+
+    nombre = models.CharField(max_length=50)
+    costo = models.DecimalField(max_digits=6, decimal_places=2)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    cantidad = models.PositiveIntegerField(default=0)
--- inventario/tests.py
+++ inventario/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from sistema.models import Sistema
+from .models import Articulo
+from .exceptions import CantidadError
 
-# Create your tests here.
+
+class ArticuloTestCase(TestCase):
+
+    def setUp():
+        sistema = Sistema.objects.create(name="Test")
+        Articulo.for_sistema.create(
+            sistema=sistema,
+            nombre="Computadora",
+            costo=100, precio=125)
+
+    def test_entrada(self):
+        articulo = Articulo.objects.get(nombre="Computadora")
+        cantidad_actual = articulo.cantidad
+        articulo.entrada(50)
+        self.assertEqual(articulo.cantidad, cantidad_actual + 50)
+
+    def test_salida(self):
+        articulo = Articulo.objects.get(nombre="Computadora")
+        cantidad_actual = articulo.cantidad
+        with self.assertRaises(CantidadError):
+            articulo.salida(50)
+
+        articulo.entrada(25)
+        self.assertEqual(articulo.cantidad, cantidad_actual + 25)
+        cantidad_actual = articulo.cantidad
+
+        articulo.salida(10)
+        self.assertEqual(articulo.cantidad, cantidad_actual - 10)
