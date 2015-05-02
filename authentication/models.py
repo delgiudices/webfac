a11248081912132cb451a906ddc7c6a2141b1226
Luis Del Giudice <luis.dg19@gmail.com>
2015-05-02 12:08:42 -0400

    Removed socket server

--- authentication/models.py
+++ authentication/models.py
@@ -1,13 +1,12 @@
 from django.contrib.auth import models as authentication_models
 from sistema.models import SistemaModel, SistemaModelManager
-from django.dispatch import receiver
-from django.db.models.signals import pre_save
 
 # Create your models here.
 
 
 class UserManager(authentication_models.UserManager, SistemaModelManager):
-    """ Nothing
+    """
+    Nothing
     """
 
 
